9f352bb0c410210c7afdd32d9d3b69c007889410
ItaloOliveiraF <44750425+ItaloOliveiraF@users.noreply.github.com>
2019-11-04 21:13:45 -0300

    Att dia04/11

--- clienteTCP.py3.py
+++ clienteTCP.py3.py
@@ -14,8 +14,11 @@ serverName = 'localhost' # ip do servidor
 serverPort = 65000 # porta a se conectar
 clientSocket = socket(AF_INET,SOCK_STREAM) # criacao do socket TCP
 clientSocket.connect((serverName, serverPort)) # conecta o socket ao servidor
-
-sentence = input('Digite o texto em letras minusculas: ')
+nickSolicitacao=clientSocket.recv(1024)
+nickname=raw_input(nickSolicitacao)
+clientSocket.send(nickname.encode('utf-8'))
+newUserEntrou=clientSocket.recv(1024)
+print(newUserEntrou)
 clientSocket.send(sentence.encode('utf-8')) # envia o texto para o servidor
 modifiedSentence = clientSocket.recv(1024) # recebe do servidor a resposta
 print ('O servidor (\'%s\', %d) respondeu com: %s' % (serverName, serverPort, modifiedSentence.decode('utf-8')))
--- servidorTCP.py3.py
+++ servidorTCP.py3.py
@@ -15,13 +15,18 @@ serverPort = 65000 # porta a se conectar
 serverSocket = socket(AF_INET,SOCK_STREAM) # criacao do socket TCP
 serverSocket.bind((serverName,serverPort)) # bind do ip do servidor com a porta
 serverSocket.listen(1) # socket pronto para 'ouvir' conexoes
-print ('Servidor TCP esperando conexoes na porta %d ...' % (serverPort))
+print ('Sala de chat iniciada na porta %d ...' % (serverPort))
 while 1:
   connectionSocket, addr = serverSocket.accept() # aceita as conexoes dos clientes
+  connectionSocket.send("Escolha seu nickname:".encode('utf-8'))
+  nickname = connectionSocket.recv(1024)
+  newUserEntrou='%s entrou...' % (nickname)
+  print (newUserEntrou)
+  connectionSocket.send(newUserEntrou)
   sentence = connectionSocket.recv(1024) # recebe dados do cliente
   sentence = sentence.decode('utf-8')
-  capitalizedSentence = sentence.upper() # converte em letras maiusculas
-  print ('Cliente %s enviou: %s, transformando em: %s' % (addr, sentence, capitalizedSentence))
-  connectionSocket.send(capitalizedSentence.encode('utf-8')) # envia para o cliente o texto transformado
+  print ('%s escreveu: %s' % (nickname, sentence))
+  connectionSocket.send(sentence) # envia para o cliente o texto transformado
   connectionSocket.close() # encerra o socket com o cliente
+  print('%s saiu!' % (nickname))
 serverSocket.close() # encerra o socket do servidor
