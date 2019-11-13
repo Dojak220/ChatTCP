25a1bc63a0344b7f8c64540e1f51692325304798
ItaloOliveiraF <44750425+ItaloOliveiraF@users.noreply.github.com>
2019-11-13 14:24:14 -0300

    Add files via upload
    Att 13/11

--- clienteTCP.py3.py
+++ clienteTCP.py3.py
@@ -8,19 +8,62 @@
 
 # importacao das bibliotecas
 from socket import *
+import threading as th
+from user import User
+
+def nicknameSelect(clientSocket):
+    nickSolicitacao=clientSocket.recv(1024).decode('utf-8')
+    nickname=input(nickSolicitacao)
+    clientSocket.send(nickname.encode('utf-8'))
+    return nickname
+    
+def quitUser(clientSocket):
+    clientSocket.close() # encerramento o socket do cliente
+    
+def sendMessage(clientSocket):
+    while(1):
+        sentence=input()
+        clientSocket.send(sentence.encode('utf-8'))
+
+def readMessage(clientSocket):
+    while(1):    
+        message = clientSocket.recv(1024).decode('utf-8')
+        print(message)
+    
+
 
 # definicao das variaveis
 serverName = 'localhost' # ip do servidor
 serverPort = 65000 # porta a se conectar
 clientSocket = socket(AF_INET,SOCK_STREAM) # criacao do socket TCP
 clientSocket.connect((serverName, serverPort)) # conecta o socket ao servidor
-nickSolicitacao=clientSocket.recv(1024)
-nickname=raw_input(nickSolicitacao)
-clientSocket.send(nickname.encode('utf-8'))
-newUserEntrou=clientSocket.recv(1024)
+
+nicknameSelect(clientSocket)
+newUserEntrou=clientSocket.recv(1024).decode('utf-8')
 print(newUserEntrou)
-clientSocket.send(sentence.encode('utf-8')) # envia o texto para o servidor
-modifiedSentence = clientSocket.recv(1024) # recebe do servidor a resposta
-print ('O servidor (\'%s\', %d) respondeu com: %s' % (serverName, serverPort, modifiedSentence.decode('utf-8')))
-clientSocket.close() # encerramento o socket do cliente
+
+send = th.Thread(target=readMessage, args=(clientSocket, ))
+send.start()
+read = th.Thread(target=sendMessage, args=(clientSocket, ))
+read.start()
+
+'''
+    sentence=input("Digite sua mensagem:")
+    sentMessage(clientSocket, sentence)
+     # envia o texto para o servidor
+'''
+#quitUser(clientSocket)
+
+# o servidor deve iniciar
+# Seridor fica aguardando novas conexões constantemente
+# o cliente deve se conectar ao servidor
+# o servidor deve enviar uma solicitação do nickname ao cliente
+# o cliente deve escolher um nickname e enviar ao servidor
+# o servidor deve receber o nickname
+# o servidor deve salvar o <nick, ip, porta> em uma lista de usuários
+# o servidor envia para todos os usuários que um novo usuário entrou
+# o cliente deve receber a mensagem de que um novo usuário entrou
+# O servidor deve enviar para todos os usuários as mensagens enviadas e quem enviou
+# O cliente deve poder mandar mensagem a todo momento
+# O cliente deve receber mensagens novas a todo momento
 
--- servidorTCP.py3.py
+++ servidorTCP.py3.py
@@ -8,25 +8,81 @@
 
 # importacao das bibliotecas
 from socket import * # sockets
+import threading as th
+from user import User
 
+
+def fluxoCliente(sock, addr):
+  myUser=newUser(sock, addr)
+  newUserEntrou='%s entrou...' % myUser.getNick()
+  print(newUserEntrou)
+  sendMessage(connectionSocket,  newUserEntrou)
+  
+  newMessage=th.Thread(target = receiveMessages, args=(connectionSocket,myUser, ))
+  newMessage.start()
+  
+  
+  '''
+  message = 'None'
+  while message != 'quit':
+    message = connectionSocket.recv(1024).decode('utf-8')
+    print(novoUsuario.getNick+': '+message)
+    
+    if message.find('(') != -1 and message.find(')') != -1:
+      comando = message.split('(')
+      if comando[0] == 'privado':
+        privateMessage = comando[1].split(')')
+        
+      if comando[0] == 'lista':
+  '''
+    
+       
+def nickChoice(connectionSocket):
+  sendMessage(connectionSocket, "Escolha seu nickname:")
+  nickname = connectionSocket.recv(1024).decode('utf-8')
+  return nickname
+
+def newUser(connectionSocket, addr):
+  nick=nickChoice(connectionSocket)
+  newUser=User(nick, addr[0], addr[1])
+  return newUser
+  
+def receiveMessages(connectionSocket, myUser):
+  while(1):
+    message=connectionSocket.recv(1024).decode('utf-8')
+    print(myUser.getNick() + ":" + message)
+    sendMessage(connectionSocket, message)
+  
+
+def sendMessage(connectionSocket, message):
+  connectionSocket.send(message.encode('utf-8'))
+    
+    
+  
+  
 # definicao das variaveis
 serverName = '' # ip do servidor (em branco)
 serverPort = 65000 # porta a se conectar
 serverSocket = socket(AF_INET,SOCK_STREAM) # criacao do socket TCP
 serverSocket.bind((serverName,serverPort)) # bind do ip do servidor com a porta
 serverSocket.listen(1) # socket pronto para 'ouvir' conexoes
+
 print ('Sala de chat iniciada na porta %d ...' % (serverPort))
 while 1:
   connectionSocket, addr = serverSocket.accept() # aceita as conexoes dos clientes
-  connectionSocket.send("Escolha seu nickname:".encode('utf-8'))
-  nickname = connectionSocket.recv(1024)
-  newUserEntrou='%s entrou...' % (nickname)
-  print (newUserEntrou)
-  connectionSocket.send(newUserEntrou)
-  sentence = connectionSocket.recv(1024) # recebe dados do cliente
-  sentence = sentence.decode('utf-8')
-  print ('%s escreveu: %s' % (nickname, sentence))
-  connectionSocket.send(sentence) # envia para o cliente o texto transformado
-  connectionSocket.close() # encerra o socket com o cliente
-  print('%s saiu!' % (nickname))
+  t = th.Thread(target=fluxoCliente, args=(connectionSocket,addr,))
+  t.start()
+  #userList=newUser(userList, connectionSocket, addr)
+  #n=len(userList)
+  #newUserEntrou='%s entrou...' % userList[n-1][0]
+  #print(newUser)
+  #print (newUserEntrou)
+  #connectionSocket.send(newUserEntrou.encode('utf-8'))
+  #while(1):
+  #  sentence = connectionSocket.recv(1024) # recebe dados do cliente
+  #  sentence = sentence.decode('utf-8')
+  #  print ('%s escreveu: %s' % (userList[n-1][0], sentence))
+  # connectionSocket.send(sentence.encode('utf-8')) # envia para o cliente o texto transformado
+  #connectionSocket.close() # encerra o socket com o cliente
+  #print('%s saiu!' % (nickname))
 serverSocket.close() # encerra o socket do servidor
